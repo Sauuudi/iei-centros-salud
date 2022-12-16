97ecc094975fbd44ee15e021b9e48e97d7a1f360
Marceldts <marceldeltoro232@gmail.com>
2022-12-16 12:18:43 +0100

    Carga Json hecha Filtrado Busqueda hecho

--- src/app/formulario-busqueda/formulario-busqueda.component.ts
+++ src/app/formulario-busqueda/formulario-busqueda.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormularioCargaComponent } from '../formulario-carga/formulario-carga.component';
 import { MapService } from '../services/map.service';
 @Component({
   selector: 'app-formulario-busqueda',
@@ -7,16 +8,19 @@ import { MapService } from '../services/map.service';
   styleUrls: ['./formulario-busqueda.component.scss'],
 })
 export class FormularioBusquedaComponent implements OnInit {
-  
   searchForm!: FormGroup;
   mapTarget: HTMLElement;
   popup: HTMLElement;
   resultados = '';
 
-  constructor(private fb: FormBuilder, private map: MapService) {}
+  constructor(
+    private fb: FormBuilder,
+    private map: MapService,
+    private centros: FormularioCargaComponent
+  ) {}
 
   ngOnInit() {
-    this.popup = document.getElementById('popup') as HTMLElement ;
+    this.popup = document.getElementById('popup') as HTMLElement;
     this.mapTarget = document.getElementById('map') as HTMLElement;
     this.map.initializeMap(this.mapTarget, this.popup);
     this.checkForm();
@@ -33,15 +37,43 @@ export class FormularioBusquedaComponent implements OnInit {
 
   onSearch() {
     this.onCancel();
-    const busqueda : { city, postal, provincia, type } = this.searchForm.value;
-    console.log(busqueda);
-    
+    const busqueda: { city; postal; provincia; type } = this.searchForm.value;
+    // console.log(busqueda);
+    this.resultados += `Resultado de la búsqueda para tipo: ${busqueda.type}, ciudad: ${busqueda.city}, CP: ${busqueda.postal}, provincia: ${busqueda.provincia}\n`;
+
+    let est = this.centros.centros;
+    if (busqueda.type != 'Cualquiera') {
+      est = est.filter((el) => {
+        el.tipo.toLowerCase() !== busqueda.type.toLowerCase();
+      });
+    }
+    if (busqueda.city != '') {
+      est = est.filter((el) => {
+        el.localidad.toLowerCase() !== busqueda.city.toLowerCase();
+      });
+    }
+    if (busqueda.postal != '') {
+      est = est.filter((el) => {
+        el.cod_postal.toLowerCase() !== busqueda.postal.toLowerCase();
+      });
+    }
+    if (busqueda.provincia != 'Cualquiera') {
+      est = est.filter((el) => {
+        el.provincia.toLowerCase() !== busqueda.provincia.toLowerCase();
+      });
+    }
     //Añadir petición de búsqueda
     //depues de la pet mostrar los centros en resultados y en el mapa con marcadores
-    this.map.createMarker(-0.375, 39.466667, 'hospital', 'hospital del centro');
-    this.map.createMarker(-0.38, 39.466667, 'hospital', 'hospital 2');
+    est.forEach((el) => {
+      this.map.createMarker(
+        <number>(<unknown>el.longitud),
+        <number>(<unknown>el.latitud),
+        el.tipo,
+        el.nombre
+      );
+      this.resultados += `${el.nombre}, ${el.cod_postal}, ${el.descripcion}, ${el.direccion}, ${el.telefono}, ${el.tipo}\n`;
+    });
     //new Centro(), add al array que mostraremoe en el ngFor del html en resultados
-    this.resultados = 'los resultados \n';
   }
 
   onCancel() {
