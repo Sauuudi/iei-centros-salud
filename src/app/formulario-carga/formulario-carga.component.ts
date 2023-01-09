dcc3f4bffbdc6d225a7c193aa4e59911b48083d9
Marceldts <marceldeltoro232@gmail.com>
2023-01-09 10:10:57 +0100

    Checkboxes formulario de carga puestos

--- src/app/formulario-carga/formulario-carga.component.ts
+++ src/app/formulario-carga/formulario-carga.component.ts
@@ -1,22 +1,37 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import data from 'src/assets/data.json'
+import data from 'src/assets/data.json';
 
 @Component({
   selector: 'app-formulario-carga',
   templateUrl: './formulario-carga.component.html',
-  styleUrls: ['./formulario-carga.component.scss']
+  styleUrls: ['./formulario-carga.component.scss'],
 })
 export class FormularioCargaComponent {
-
   centros = data;
+  resultados = []
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  onSearch(){
+    const all = document.querySelector('#all') as HTMLInputElement
+    const baleares = document.querySelector('#baleares') as HTMLInputElement
+    const valencia = document.querySelector('#valencia') as HTMLInputElement
+    const euskadi = document.querySelector('#euskadi') as HTMLInputElement
+    console.log(euskadi.checked)
 
+    //Añadir consultas a la API dependiendo de si las constantes de arriba estan checked o no
 
- 
+    //Cambiar el texto del recuadro dependiendo de los resultados de estas peticiones
+    this.resultados.push('Error 1')
+  }
 
+  onCancel(){
+    this.resultados = []
+  }
+
+  onClear(){
+
+  }
+
+  constructor(private http: HttpClient) {}
 }
